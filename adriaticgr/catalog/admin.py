1d2bba4ff223934da012ce0aa1dea81e63f86674
Stalker3190 <74285998+Stalker3190@users.noreply.github.com>
2024-08-19 18:03:03 +0300

    Доработка бд и связка с сайтом

--- adriaticgr/catalog/admin.py
+++ adriaticgr/catalog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import City, Company, Product, CompanyProduct
 
 # Register your models here.
+
+admin.site.register(City)
+admin.site.register(Company)
+admin.site.register(Product)
+admin.site.register(CompanyProduct)
--- adriaticgr/catalog/views.py
+++ adriaticgr/catalog/views.py
@@ -1,23 +1,11 @@
 from django.shortcuts import render
-from .models import Product, CompanyProduct
+from .models import Company, Product
 
 # Create your views here.
 
 #представление для поиска товаров
 
-def search_products(request):
+def search(request):
     query = request.GET.get('q')
-    products = Product.objects.filter(name__icontains=query)
-    results = []
-
-    for product in products:
-        company_products = CompanyProduct.objects.filter(product=product)
-        for company_product in company_products:
-            results.append(company_product)
-
-    context = {
-        'results': results,
-        'query': query,
-    }
-
-    return render(request, 'search_results.html', context)
+    results = Company.objects.filter(companyproduct__product__name__icontains=query).distinct()
+    return render(request, 'catalog/search_results.html', {'results': results, 'query': query})
