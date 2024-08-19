22b33437411fe60f1f212a82d720f02f38be5d81
Stalker3190 <74285998+Stalker3190@users.noreply.github.com>
2024-08-19 18:56:51 +0300

    Исправлены ошибки подключения

--- adriaticgr/catalog/views.py
+++ adriaticgr/catalog/views.py
@@ -3,9 +3,17 @@ from .models import Company, Product
 
 # Create your views here.
 
+#представление для рендеринга основной страницы
+def index(request):
+    return render(request, 'catalog/index.html')
+
+
 #представление для поиска товаров
 
 def search(request):
     query = request.GET.get('q')
-    results = Company.objects.filter(companyproduct__product__name__icontains=query).distinct()
+    if query:
+        results = Company.objects.filter(companyproduct__product__name__icontains=query).distinct()
+    else:
+        results = Company.objects.none()
     return render(request, 'catalog/search_results.html', {'results': results, 'query': query})
