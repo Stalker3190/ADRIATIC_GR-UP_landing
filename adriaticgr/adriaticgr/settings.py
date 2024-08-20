655ec94a91cf79b0aa1067a566fe8506d8cf3e47
Stalker3190 <74285998+Stalker3190@users.noreply.github.com>
2024-08-20 14:42:28 +0300

    Стилизация панели поиска

--- adriaticgr/adriaticgr/settings.py
+++ adriaticgr/adriaticgr/settings.py
@@ -124,4 +124,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'adriaticgr/catalog/static')]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
